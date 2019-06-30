c5d505d7513a891776d86b3acfe373927140ec36
jaesun <wotjs153@naver.com>
2019-06-30 19:12:02 +0900

    Create a Project

---
+++ src/main/java/com/jaebam/srping51movie/Srping51MovieApplication.java
@@ -0,0 +1,13 @@
+package com.jaebam.srping51movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srping51MovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Srping51MovieApplication.class, args);
+    }
+
+}
