16838b8f83d8801b479f34cda40cd7cabae3c42f
jaesun <wotjs153@naver.com>
2019-06-30 21:52:22 +0900

    MySQL, Tiles, 기본 세팅 완료

--- src/main/java/com/jaebam/spring51movie/ServletInitializer.java
+++ src/main/java/com/jaebam/spring51movie/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Srping51MovieApplication.class);
+        return application.sources(Spring51MovieApplication.class);
     }
 
 }
--- src/main/java/com/jaebam/spring51movie/Spring51MovieApplication.java
+++ src/main/java/com/jaebam/spring51movie/Spring51MovieApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Srping51MovieApplication {
+public class Spring51MovieApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Srping51MovieApplication.class, args);
+        SpringApplication.run(Spring51MovieApplication.class, args);
     }
 
 }
