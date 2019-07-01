de066357a5867506e51da52e66a3e19692606b22
jaesun <wotjs153@naver.com>
2019-07-01 09:30:34 +0900

    MyBatis Mapping Xml 제거

--- src/main/java/com/jaebam/spring51movie/config/MyBatisConfig.java
+++ src/main/java/com/jaebam/spring51movie/config/MyBatisConfig.java
@@ -3,14 +3,16 @@ package com.jaebam.spring51movie.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
+@MapperScan(basePackageClasses = com.jaebam.spring51movie.mapper.AdminMapper.class)
+// basePackageClasses 해당 클래스부터 하위 패키지의 모든 클래스 스캔
+// basePackages 역할은 위와 같으나 문자열로 받기 때문에 none-Type-Safety
 public class MyBatisConfig {
 
     @Bean
@@ -18,10 +20,6 @@ public class MyBatisConfig {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource);
         factoryBean.setTypeAliasesPackage("com.jaebam.spring51movie.model");
-
-        Resource[] resource = new PathMatchingResourcePatternResolver()
-                .getResources("classpath:mapper/**/*Mapper.xml");
-        factoryBean.setMapperLocations(resource);
 
         return factoryBean.getObject();
     }
--- src/main/java/com/jaebam/spring51movie/mapper/AdminMapper.java
+++ src/main/java/com/jaebam/spring51movie/mapper/AdminMapper.java
@@ -1,9 +1,12 @@
 package com.jaebam.spring51movie.mapper;
 
 import com.jaebam.spring51movie.model.TestTable;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface AdminMapper {
+    @Select("SELECT * " +
+              "FROM test_table")
     List<TestTable> getList();
 }
